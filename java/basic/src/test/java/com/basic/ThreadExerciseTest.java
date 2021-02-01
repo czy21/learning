14e279b72d37d78a054a7b844f7c689e9cf6901a
Bruce CHEN <805899926@qq.com>
2021-02-01 12:27:26 +0800

    #

--- java/basic/src/test/java/com/basic/ThreadExerciseTest.java
+++ java/basic/src/test/java/com/basic/ThreadExerciseTest.java
@@ -8,13 +8,15 @@ public class ThreadExerciseTest {
 
     class Task implements Callable<Integer> {
         private Integer taskID;
+
         public Task(Integer taskID) {
             this.taskID = taskID;
         }
+
         public Integer call() throws Exception {
 //            if(taskID.equals(3))
 //                Thread.sleep(10000);
-            System.out.println("任务["+taskID+"]开始执行");
+            System.out.println("任务[" + taskID + "]开始执行");
             return taskID;
         }
     }
@@ -23,12 +25,12 @@ public class ThreadExerciseTest {
     public void single() {
         //创建一个线程池
         ExecutorService pools = Executors.newFixedThreadPool(5);
-        CompletionService<Integer> s = new ExecutorCompletionService<Integer>(pools);
+        CompletionService<Integer> s = new ExecutorCompletionService<>(pools);
         //创建多个有返回值的任务
-        for(int i = 0 ; i <= 10 ; i++){
+        for (int i = 0; i <= 10; i++) {
             s.submit(new Task(i));
         }
-        for(int i = 0 ; i <= 10 ; i++){
+        for (int i = 0; i <= 10; i++) {
             try {
                 Future<Integer> f = s.take();
                 System.out.println(f.get());
