db73211cc24950f4d4d7e399fdbfc11978cd68d7
Bruce CHEN <805899926@qq.com>
2021-07-19 08:38:41 +0800

    #

--- java/basic/src/main/java/com/basic/ThreadExercise.java
+++ java/basic/src/main/java/com/basic/ThreadExercise.java
@@ -1,7 +1,9 @@
 package com.basic;
 
+import com.clearning.entity.po.UserPO;
 import lombok.Data;
 import org.openjdk.jol.info.ClassLayout;
+import org.openjdk.jol.info.GraphLayout;
 
 public class ThreadExercise {
 
@@ -11,10 +13,11 @@ public class ThreadExercise {
         private String name;
         private Integer age;
         private int value;
+
         public void plus() {
             int i = 3;
             int j = 9;
-            value=i+j;
+            value = i + j;
         }
     }
 
@@ -24,10 +27,22 @@ public class ThreadExercise {
 
 //        Map<String, Object> m1 = new HashMap<>();
 //        m1.put("name", 1);
-        Object1 o1 = new Object1();
-        o1.plus();
-        o1.setName("你是谁");
-        System.out.println(ClassLayout.parseInstance(o1.getValue()).toPrintable());
+        Object o1 = new Object();
+//        o1.plus();
+//        o1.setName("你是谁");
+        System.out.println(ClassLayout.parseInstance(o1).toPrintable());
 
+        synchronized (o1) {
+            System.out.println(ClassLayout.parseInstance(o1).toPrintable());
+        }
+
+
+//        UserPO u1=new UserPO();
+//
+//        System.out.println(ClassLayout.parseInstance(u1).toPrintable());
+//
+//        synchronized (u1){
+//            System.out.println(ClassLayout.parseInstance(u1).toPrintable());
+//        }
     }
 }
