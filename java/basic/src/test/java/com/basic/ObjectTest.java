5fdb52dc319ea857bdfdf65966c63ea5649c2248
Bruce CHEN <805899926@qq.com>
2021-07-20 19:58:04 +0800

    #

--- java/basic/src/test/java/com/basic/ObjectTest.java
+++ java/basic/src/test/java/com/basic/ObjectTest.java
@@ -1,8 +1,11 @@
 package com.basic;
 
 import lombok.Data;
+import org.apache.commons.lang3.StringUtils;
+import org.apache.lucene.util.RamUsageEstimator;
 import org.junit.jupiter.api.Test;
 import org.openjdk.jol.info.ClassLayout;
+import org.openjdk.jol.info.GraphLayout;
 
 
 public class ObjectTest {
@@ -22,5 +25,17 @@ public class ObjectTest {
     public void test1() {
         Object1 o1 = new Object1();
         System.out.println(ClassLayout.parseInstance(o1).toPrintable());
+    }
+
+    /*
+     * String 对象布局
+     */
+    @Test
+    public void test2() {
+        String str1 = "n";
+        System.out.println(StringUtils.join("ClassLayout info: ", ClassLayout.parseInstance(str1).toPrintable()));
+        System.out.println(StringUtils.join("GraphLayout info: ", GraphLayout.parseInstance(str1).toPrintable()));
+        System.out.println(StringUtils.join("GraphLayout size: ", GraphLayout.parseInstance(str1).toPrintable()));
+        System.out.println(StringUtils.join("RamUsageEstimator size: ", RamUsageEstimator.sizeOf(str1)));
     }
 }
