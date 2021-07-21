e962131d01b67167ab9f74099ebbb22e86c41a2d
Bruce CHEN <805899926@qq.com>
2021-07-21 10:25:18 +0800

    #

--- java/basic/src/test/java/com/basic/ObjectTest.java
+++ java/basic/src/test/java/com/basic/ObjectTest.java
@@ -1,10 +1,8 @@
 package com.basic;
 
 import lombok.Data;
-import org.apache.commons.lang3.StringUtils;
 import org.junit.jupiter.api.Test;
 import org.openjdk.jol.info.ClassLayout;
-import org.openjdk.jol.info.GraphLayout;
 
 
 public class ObjectTest {
@@ -24,26 +22,5 @@ public class ObjectTest {
     public void test1() {
         Object1 o1 = new Object1();
         System.out.println(ClassLayout.parseInstance(o1).toPrintable());
-    }
-
-    /*
-     * String 对象布局 及值的内存占用情况
-     * .class底层使用unicode编码 英文字符占用1b 汉字占用2b
-     */
-    @Test
-    public void testStr1() {
-        String str1 = "y";
-        System.out.println(StringUtils.join("ClassLayout info: ", ClassLayout.parseInstance(str1).toPrintable()));
-        System.out.println(StringUtils.join("GraphLayout info: ", GraphLayout.parseInstance(str1).toPrintable()));
-    }
-
-    /*
-    * String 定义的两种方式
-    */
-    @Test
-    public void testStr2() {
-        String str1 = "ni"; //存入常量池中
-        String str2 = new String("ni"); //存入堆中
-        System.out.println(str1 == str2);
     }
 }
