d1b210a586250c0cb7090d7229b738d2721e29a5
Bruce CHEN <805899926@qq.com>
2021-07-25 13:07:43 +0800

    #

--- java/basic/src/test/java/com/basic/ThreadTest.java
+++ java/basic/src/test/java/com/basic/ThreadTest.java
@@ -17,125 +17,6 @@ import java.util.stream.Collectors;
 
 public class ThreadTest {
 
-    @Data
-    static class Task implements Callable<Integer> {
-        private Integer taskID;
-
-        public Task(Integer taskID) {
-            this.taskID = taskID;
-        }
-
-        public Integer call() throws Exception {
-//            if(taskID.equals(3))
-//                Thread.sleep(10000);
-            System.out.println("任务[" + taskID + "]开始执行");
-            return taskID;
-        }
-    }
-
-    @Data
-    static class Object1 {
-        private int count;
-        private AtomicInteger atomicCount = new AtomicInteger();
-
-        public synchronized void syncIncrease() {
-            try {
-                Thread.sleep(3);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-            count++;
-        }
-
-        public void casIncrease() {
-            atomicCount.getAndIncrement();
-        }
-
-    }
-
-    /*
-     * 使用synchronized解决多线程操作导致得线程不安全
-     */
-    @SneakyThrows
-    @Test
-    public void test1() {
-        Object1 o1 = new Object1();
-        int loopSize = 1000;
-        List<Thread> threads = List.of(
-                getThread(o1, loopSize, Object1::syncIncrease),
-                getThread(o1, loopSize, Object1::syncIncrease)
-        );
-
-        LocalDateTime startTime = LocalDateTime.now();
-        threads.forEach(Thread::start);
-        for (var t : threads) {
-            t.join();
-        }
-        LocalDateTime endTime = LocalDateTime.now();
-        int increaseValue = o1.getCount();
-        System.out.println("loop size: " + loopSize);
-        System.out.println("increase value: " + increaseValue);
-        System.out.println("threads: " + threads.size());
-        System.out.println("timeout: " + Duration.between(startTime, endTime).toMillis() + "ms");
-        Assertions.assertEquals(loopSize * threads.size(), increaseValue);
-    }
-
-    private Thread getThread(Object1 t, int loopSize, Consumer<Object1> func) {
-        return new Thread(() -> {
-            for (int i = 0; i < loopSize; i++) {
-                func.accept(t);
-            }
-        });
-    }
-
-    /*
-     * 使用cas解决多线程操作导致得线程不安全
-     */
-    @SneakyThrows
-    @Test
-    public void test2() {
-        Object1 o1 = new Object1();
-        int loopSize = 1000;
-        List<Thread> threads = List.of(
-                getThread(o1, loopSize, Object1::casIncrease),
-                getThread(o1, loopSize, Object1::casIncrease)
-        );
-
-        LocalDateTime startTime = LocalDateTime.now();
-        threads.forEach(Thread::start);
-        for (var t : threads) {
-            t.join();
-        }
-        LocalDateTime endTime = LocalDateTime.now();
-        int increaseValue = o1.getAtomicCount().get();
-        System.out.println("loop size: " + loopSize);
-        System.out.println("increase value: " + increaseValue);
-        System.out.println("threads: " + threads.size());
-        System.out.println("timeout: " + Duration.between(startTime, endTime).toMillis() + "ms");
-        Assertions.assertEquals(loopSize * threads.size(), increaseValue);
-    }
-
-    @Test
-    public void single() {
-        //创建一个线程池
-        ExecutorService pools = Executors.newFixedThreadPool(5);
-        CompletionService<Integer> s = new ExecutorCompletionService<>(pools);
-        //创建多个有返回值的任务
-        for (int i = 0; i <= 10; i++) {
-            s.submit(new Task(i));
-        }
-        for (int i = 0; i <= 10; i++) {
-            try {
-                Future<Integer> f = s.take();
-                System.out.println(f.get());
-            } catch (InterruptedException | ExecutionException e) {
-                e.printStackTrace();
-            }
-        }
-        pools.shutdown();
-
-    }
-
     @Test
     public void pool1() throws InterruptedException, ExecutionException {
         int cores = Runtime.getRuntime().availableProcessors();
@@ -159,7 +40,7 @@ public class ThreadTest {
     }
 
     @Test
-    public void test3() throws InterruptedException {
+    public void pool2() throws InterruptedException {
         List<String> strs = new ArrayList<>();
         LocalDateTime loopStartTime = LocalDateTime.now();
         for (int i = 0; i < 10; i++) {
