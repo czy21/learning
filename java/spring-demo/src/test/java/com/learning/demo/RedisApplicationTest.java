51a14fe5186692411e909f7abe902fb707a6a733
Bruce CHEN <805899926@qq.com>
2022-04-16 17:13:50 +0800

    # init module for spring

---
+++ java/spring-demo/src/test/java/com/learning/demo/RedisApplicationTest.java
@@ -0,0 +1,7 @@
+package com.learning.demo;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class RedisApplicationTest {
+
+}
---
+++ java/spring-pulsar/src/test/java/com/learning/pulsar/PulsarApplicationTest.java
@@ -0,0 +1,7 @@
+package com.learning.pulsar;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PulsarApplicationTest {
+
+}
