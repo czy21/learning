dd58805ac7654a5a3a610092fe1cf215e85ede52
Bruce Chen <805899926@qq.com>
2022-04-20 19:01:17 +0800

    #

--- java/spring-redis/src/main/java/com/learning/redis/config/RedisConfigure.java
+++ java/spring-redis/src/main/java/com/learning/redis/config/RedisConfigure.java
@@ -1,10 +1,6 @@
 package com.learning.redis.config;
 
-import org.springframework.cache.CacheManager;
 import org.springframework.cache.annotation.EnableCaching;
-import org.springframework.cache.ehcache.EhCacheCacheManager;
-import org.springframework.cache.support.SimpleCacheManager;
-import org.springframework.context.annotation.Bean;
 
 @EnableCaching
 public class RedisConfigure {
