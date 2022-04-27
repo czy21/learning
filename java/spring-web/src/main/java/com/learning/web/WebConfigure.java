783be9a1512c8e73a408fa1b11bdfd7b221c8bd4
Bruce CHEN <805899926@qq.com>
2022-04-27 16:16:49 +0800

    #

--- java/spring-web/src/main/java/com/learning/web/WebConfigure.java
+++ java/spring-web/src/main/java/com/learning/web/WebConfigure.java
@@ -1,8 +1,13 @@
 package com.learning.web;
 
 import com.learning.web.inteceptor.DefaultInterceptor;
+import io.github.mweirauch.micrometer.jvm.extras.ProcessMemoryMetrics;
+import io.github.mweirauch.micrometer.jvm.extras.ProcessThreadMetrics;
+import io.micrometer.core.instrument.MeterRegistry;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.actuate.autoconfigure.metrics.MeterRegistryCustomizer;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
@@ -15,5 +20,13 @@ public class WebConfigure implements WebMvcConfigurer {
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(new DefaultInterceptor());
+    }
+
+    @Bean
+    MeterRegistryCustomizer<MeterRegistry> meterRegistryCustomizer() {
+        return (registry) -> {
+            new ProcessMemoryMetrics().bindTo(registry);
+            new ProcessThreadMetrics().bindTo(registry);
+        };
     }
 }
