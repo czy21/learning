b69c9a05ab617331b5453a4ed8864fdc9fee6892
Bruce CHEN <805899926@qq.com>
2022-05-01 18:07:10 +0800

    #

--- java/spring-demo/src/main/java/com/learning/graphql/DemoApplication.java
+++ java/spring-demo/src/main/java/com/learning/graphql/DemoApplication.java
@@ -1,4 +1,4 @@
-package com.learning.demo;
+package com.learning.graphql;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- java/spring-web/src/test/java/com/learning/demo/RedisApplicationTest.java
+++
@@ -1,7 +0,0 @@
-package com.learning.demo;
-
-import static org.junit.jupiter.api.Assertions.*;
-
-class RedisApplicationTest {
-
-}
