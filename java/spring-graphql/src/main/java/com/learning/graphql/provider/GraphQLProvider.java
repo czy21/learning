1b3b04d6c83292ecc2ba7656b2325bfdb0f46840
Bruce CHEN <805899926@qq.com>
2022-05-02 08:14:45 +0800

    #

--- java/spring-graphql/src/main/java/com/learning/graphql/provider/GraphQLProvider.java
+++ java/spring-graphql/src/main/java/com/learning/graphql/provider/GraphQLProvider.java
@@ -1,42 +1,51 @@
 package com.learning.graphql.provider;
 
-import com.google.common.base.Charsets;
-import com.google.common.io.Resources;
 import graphql.GraphQL;
 import graphql.schema.GraphQLSchema;
-import org.apache.commons.io.IOUtils;
+import graphql.schema.idl.RuntimeWiring;
+import graphql.schema.idl.SchemaGenerator;
+import graphql.schema.idl.SchemaParser;
+import graphql.schema.idl.TypeDefinitionRegistry;
 import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Component;
 
-import javax.annotation.PostConstruct;
-import java.io.IOException;
 import java.io.InputStream;
-import java.net.URL;
 import java.nio.charset.StandardCharsets;
+
+import static graphql.schema.idl.TypeRuntimeWiring.newTypeWiring;
 
 @Component
 public class GraphQLProvider {
+    GraphQLDataFetcher graphQLDataFetcher;
 
-    private GraphQL graphQL;
+    public GraphQLProvider(GraphQLDataFetcher graphQLDataFetcher) {
+        this.graphQLDataFetcher = graphQLDataFetcher;
+    }
 
     @Bean
     public GraphQL graphQL() {
-        return graphQL;
-    }
-
-    @PostConstruct
-    public void init() throws IOException {
         try (InputStream schemaStream = getClass().getClassLoader().getResourceAsStream("schema.graphqls")) {
             String sdl = schemaStream != null ? new String(schemaStream.readAllBytes(), StandardCharsets.UTF_8) : "";
             GraphQLSchema graphQLSchema = buildSchema(sdl);
-            this.graphQL = GraphQL.newGraphQL(graphQLSchema).build();
+            return GraphQL.newGraphQL(graphQLSchema).build();
         } catch (Exception e) {
             throw new RuntimeException(e);
         }
     }
 
     private GraphQLSchema buildSchema(String sdl) {
-        // TODO: we will create the schema here later
-        return null;
+        TypeDefinitionRegistry typeRegistry = new SchemaParser().parse(sdl);
+        RuntimeWiring runtimeWiring = buildWiring();
+        SchemaGenerator schemaGenerator = new SchemaGenerator();
+        return schemaGenerator.makeExecutableSchema(typeRegistry, runtimeWiring);
+    }
+
+    private RuntimeWiring buildWiring() {
+        return RuntimeWiring.newRuntimeWiring()
+                .type(newTypeWiring("Query")
+                        .dataFetcher("bookById", graphQLDataFetcher.getBookByIdDataFetcher()))
+                .type(newTypeWiring("Book")
+                        .dataFetcher("author", graphQLDataFetcher.getAuthorDataFetcher()))
+                .build();
     }
 }
