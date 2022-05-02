09df762b3a921903c664dea7c8ff2a22d7feb8c3
Bruce CHEN <805899926@qq.com>
2022-05-02 16:00:20 +0800

    #

---
+++ java/spring-graphql/src/main/java/com/learning/graphql/config/GraphQLConfig.java
@@ -0,0 +1,67 @@
+package com.learning.graphql.config;
+
+import com.learning.graphql.provider.GraphQLDataFetcher;
+import graphql.GraphQL;
+import graphql.schema.GraphQLSchema;
+import graphql.schema.idl.RuntimeWiring;
+import graphql.schema.idl.SchemaGenerator;
+import graphql.schema.idl.SchemaParser;
+import graphql.schema.idl.TypeDefinitionRegistry;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.Resource;
+
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import static graphql.schema.idl.TypeRuntimeWiring.newTypeWiring;
+
+@EnableConfigurationProperties(GraphQLProperties.class)
+@Configuration
+public class GraphQLConfig {
+    GraphQLProperties graphQLProperties;
+
+    public GraphQLConfig(GraphQLProperties graphQLProperties) {
+        this.graphQLProperties = graphQLProperties;
+    }
+
+    @Bean
+    public GraphQL graphQL(GraphQLDataFetcher graphQLDataFetcher) {
+        TypeDefinitionRegistry typeDefinitionRegistry = new TypeDefinitionRegistry();
+        SchemaParser schemaParser = new SchemaParser();
+        SchemaGenerator schemaGenerator = new SchemaGenerator();
+        Resource[] schemaResources = graphQLProperties.resolveSchemaLocations();
+        parseSchemaResources(typeDefinitionRegistry, schemaParser, schemaResources);
+        RuntimeWiring runtimeWiring = buildWiring(graphQLDataFetcher);
+        GraphQLSchema graphQLSchema = schemaGenerator.makeExecutableSchema(typeDefinitionRegistry, runtimeWiring);
+        return GraphQL.newGraphQL(graphQLSchema).build();
+    }
+
+    public void parseSchemaResources(TypeDefinitionRegistry typeDefinitionRegistry,
+                                     SchemaParser schemaParser,
+                                     Resource... schemaResources) {
+        for (Resource r : schemaResources) {
+            try {
+                typeDefinitionRegistry.merge(schemaParser.parse(r.getInputStream()));
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
+
+    private RuntimeWiring buildWiring(GraphQLDataFetcher graphQLDataFetcher) {
+        return RuntimeWiring.newRuntimeWiring()
+                .type(newTypeWiring("Query")
+                        .dataFetcher("books", graphQLDataFetcher.findAllBook())
+                        .dataFetcher("bookById", graphQLDataFetcher.findBookById())
+                )
+                .type(newTypeWiring("Book")
+                        .dataFetcher("author", graphQLDataFetcher.findAuthorById())
+                )
+                .build();
+    }
+
+}
--- java/spring-graphql/src/main/java/com/learning/graphql/provider/GraphQLProvider.java
+++ java/spring-graphql/src/main/java/com/learning/graphql/provider/GraphQLProvider.java
@@ -22,48 +22,5 @@ import static graphql.schema.idl.TypeRuntimeWiring.newTypeWiring;
 
 @Component
 public class GraphQLProvider {
-    GraphQLDataFetcher graphQLDataFetcher;
-    ApplicationContext applicationContext;
 
-    public GraphQLProvider(GraphQLDataFetcher graphQLDataFetcher,
-                           ApplicationContext applicationContext) {
-        this.graphQLDataFetcher = graphQLDataFetcher;
-        this.applicationContext = applicationContext;
-    }
-
-    @Bean
-    public GraphQL graphQL() {
-        TypeDefinitionRegistry typeRegistry = new TypeDefinitionRegistry();
-        SchemaParser schemaParser = new SchemaParser();
-        SchemaGenerator schemaGenerator = new SchemaGenerator();
-        try {
-            Resource[] resources = applicationContext.getResources("classpath*:" + "graphql/**.graphql");
-            List<TypeDefinitionRegistry> typeDefinitionRegistries = Arrays.stream(resources)
-                    .map(t -> {
-                        try {
-                            return schemaParser.parse(t.getInputStream());
-                        } catch (IOException e) {
-                            throw new RuntimeException(e);
-                        }
-                    }).collect(Collectors.toList());
-            typeDefinitionRegistries.forEach(typeRegistry::merge);
-            RuntimeWiring runtimeWiring = buildWiring();
-            GraphQLSchema graphQLSchema = schemaGenerator.makeExecutableSchema(typeRegistry, runtimeWiring);
-            return GraphQL.newGraphQL(graphQLSchema).build();
-        } catch (Exception e) {
-            throw new RuntimeException(e);
-        }
-    }
-
-    private RuntimeWiring buildWiring() {
-        return RuntimeWiring.newRuntimeWiring()
-                .type(newTypeWiring("Query")
-                        .dataFetcher("books", graphQLDataFetcher.findAllBook())
-                        .dataFetcher("bookById", graphQLDataFetcher.findBookById())
-                )
-                .type(newTypeWiring("Book")
-                        .dataFetcher("author", graphQLDataFetcher.findAuthorById())
-                )
-                .build();
-    }
 }
