4be5a55d49191818c48ef40c7a57707fa111a4f0
Bruce Chen <805899926@qq.com>
2022-06-03 15:25:45 +0800

    #

---
+++ java/infrastructure/src/main/java/com/learning/infranstructure/exception/BusinessException.java
@@ -0,0 +1,24 @@
+package com.learning.infranstructure.exception;
+
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+@EqualsAndHashCode(callSuper = true)
+@Data
+public class BusinessException extends RuntimeException {
+
+    String code;
+    String message;
+
+    public BusinessException(ExceptionMessage em, String message) {
+        super(message == null ? em.getMessage() : message);
+        this.code = em.getCode();
+        this.message = super.getMessage();
+    }
+
+    public BusinessException(ExceptionMessage em) {
+        this(em, null);
+    }
+
+}
---
+++ java/spring-web/src/main/java/com/learning/web/exception/BusinessException.java
@@ -0,0 +1,25 @@
+package com.learning.web.exception;
+
+
+import com.learning.infranstructure.exception.ExceptionMessage;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+@EqualsAndHashCode(callSuper = true)
+@Data
+public class BusinessException extends RuntimeException {
+
+    String code;
+    String message;
+
+    public BusinessException(ExceptionMessage em, String message) {
+        super(message == null ? em.getMessage() : message);
+        this.code = em.getCode();
+        this.message = super.getMessage();
+    }
+
+    public BusinessException(ExceptionMessage em) {
+        this(em, null);
+    }
+
+}
