24812b7db3085c4e8ad519c2e360218a3227c611
Bruce CHEN <805899926@qq.com>
2022-08-07 22:49:30 +0800

    # init dotnet mvc

---
+++ dotnet/Learning/Controllers/HomeController.cs
@@ -0,0 +1,23 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using Learning.Models;
+
+namespace Learning.Controllers;
+
+public class HomeController : Controller
+{
+    private readonly ILogger<HomeController> _logger;
+
+    public HomeController(ILogger<HomeController> logger)
+    {
+        _logger = logger;
+    }
+
+    [HttpGet("list")]
+    public Task<List<Dictionary<string, object>>> List()
+    {
+        var dic = new Dictionary<string, object> { { "name", "czy" } };
+        var list = new List<Dictionary<string, object>> { new Dictionary<string, object>() { { "name", "czy" }, { "age", "26" } } };
+        return Task.FromResult(list);
+    }
+}
