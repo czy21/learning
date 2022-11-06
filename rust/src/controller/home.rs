a27f76336670cbab2f94ee56261789a5b040b66a
Bruce CHEN <805899926@qq.com>
2022-11-06 23:14:50 +0800

    #

--- rust/src/controller/home.rs
+++ rust/src/controller/home.rs
@@ -1,4 +1,4 @@
-use actix_web::{Responder, web,get};
+use actix_web::{get, web, Responder};
 
 #[get("/hello/{name}")]
 pub async fn greet(name: web::Path<String>) -> impl Responder {
--- rust/src/main.rs
+++ rust/src/main.rs
@@ -4,10 +4,9 @@ use actix_web::{App, HttpServer};
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| {
-        App::new().service(controller::home::greet)
-    })
-        .bind(("127.0.0.1", 8080))?
-        .run()
+    HttpServer::new(|| App::new()
+        .service(controller::home::greet))
+        .bind(("127.0.0.1", 8080))
+        ?.run()
         .await
 }
