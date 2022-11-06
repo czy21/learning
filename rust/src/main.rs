d64bfd373520f2391c6365c8b1b9e0755b8fa5a3
Bruce CHEN <805899926@qq.com>
2022-11-06 15:53:15 +0800

    #

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -1,12 +1,11 @@
 pub mod controller;
 
 use actix_web::{App, HttpServer};
-use crate::controller::home::greet;
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     HttpServer::new(|| {
-        App::new().service(greet)
+        App::new().service(controller::home::greet)
     })
         .bind(("127.0.0.1", 8080))?
         .run()
