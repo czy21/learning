cf8f39bcb671a7bafaa8c38faee9add91f5ce06c
Bruce CHEN <805899926@qq.com>
2022-11-09 23:14:45 +0800

    #

--- go/src/lee/sum_test.go
+++ go/src/lee/sum_test.go
@@ -24,22 +24,23 @@ type ListNode struct {
 }
 
 func addTwoNumbers(l1 *ListNode, l2 *ListNode) *ListNode {
-	l := l1
-	var k []int
+	var prep *ListNode
+	var next *ListNode
 	for {
-		k = append(k, l.Val)
-		if l.Next != nil {
-			l = l.Next
+		if l1 != nil {
+			next = l1.Next
+			l1.Next = prep
+			prep = l1
+			l1 = next
 		} else {
 			break
 		}
 	}
-	fmt.Println(k)
 	return &ListNode{}
 }
 
 func TestSum2(t *testing.T) {
-	l1 := ListNode{Val: 2, Next: &ListNode{Val: 4, Next: &ListNode{Val: 3}}}
+	l1 := ListNode{Val: 1, Next: &ListNode{Val: 2, Next: &ListNode{Val: 3}}}
 	l2 := ListNode{Val: 5, Next: &ListNode{Val: 6, Next: &ListNode{Val: 4}}}
 	addTwoNumbers(&l1, &l2)
 }
