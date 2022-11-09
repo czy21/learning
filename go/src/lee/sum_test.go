964566de32eb3ce4f97ad7499ffab2dd4279ce94
Bruce CHEN <805899926@qq.com>
2022-11-09 22:50:39 +0800

    #

---
+++ go/src/lee/sum_test.go
@@ -0,0 +1,34 @@
+package lee
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestSum1(t *testing.T) {
+	arr := []int{1, 2, 3, 4, 5}
+	target := 5
+	for i := 0; i < len(arr); i++ {
+		for j := i + 1; j < len(arr); j++ {
+			v := arr[i] + arr[j]
+			if v == target {
+				fmt.Println(i, j)
+			}
+		}
+	}
+}
+
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+func addTwoNumbers(l1 *ListNode, l2 *ListNode) *ListNode {
+	return &ListNode{}
+}
+
+func TestSum2(t *testing.T) {
+	l1 := ListNode{Val: 2, Next: &ListNode{Val: 4, Next: &ListNode{Val: 3}}}
+	l2 := ListNode{Val: 5, Next: &ListNode{Val: 6, Next: &ListNode{Val: 4}}}
+	addTwoNumbers(&l1, &l2)
+}
