d091f9979d354140851404f3a9c1014e79e81d00
Bruce CHEN <805899926@qq.com>
2023-01-11 18:25:59 +0800

    #

---
+++ java/gradle-plugin/src/main/groovy/com/learning/gradle/SpringBootPlugin.groovy
@@ -0,0 +1,10 @@
+package com.learning.gradle
+
+import org.gradle.api.Plugin
+import org.gradle.api.Project;
+
+class SpringBootPlugin implements Plugin<Project> {
+    void apply(Project p) {
+
+    }
+}
