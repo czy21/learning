850c13439e28fee8c4395420021ddc4bf0a9e20b
Bruce CHEN <805899926@qq.com>
2023-03-07 19:04:30 +0800

    #

--- rust/src/controller/user.rs
+++ rust/src/controller/user.rs
@@ -1,16 +1,36 @@
 use std::collections::HashMap;
 use actix_web::{get, web, Responder, Result, post};
 use serde::{Deserialize, Serialize};
+use serde_json::{json, Value};
 
 #[derive(Serialize, Deserialize)]
 struct User {
     name: String,
 }
 
-#[post("/user/search")]
-pub async fn search(param: web::Json<HashMap<String, String>>) -> Result<impl Responder> {
+#[post("/user/post1")]
+pub async fn post1(param: web::Json<HashMap<String, Value>>) -> Result<impl Responder> {
+    let _name = json!(param.get("name"));
     Ok(web::Json(User {
-        name: "a".to_string()
+        name: _name.to_string()
+    }))
+}
+
+#[post("/user/post2")]
+pub async fn post2(param: web::Json<HashMap<String, Value>>) -> Result<impl Responder> {
+    let _age = param.get("age").unwrap();
+    println!("{}", _age);
+    Ok(web::Json(param))
+}
+
+#[get("/user/get1")]
+pub async fn get1(query: web::Query<HashMap<String, Value>>) -> Result<impl Responder> {
+    let _name = query.get("name").unwrap();
+    let _age = query.get("age").unwrap();
+    println!("{}", _name);
+    println!("{}", _age);
+    Ok(web::Json(User {
+        name: String::from("aaa")
     }))
 }
 
--- rust/src/main.rs
+++ rust/src/main.rs
@@ -5,9 +5,11 @@ use actix_web::{App, HttpServer};
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     HttpServer::new(|| App::new()
-        .service(controller::user::search)
+        .service(controller::user::post1)
+        .service(controller::user::post2)
+        .service(controller::user::get1)
         .service(controller::user::detail))
-        .bind(("127.0.0.1", 8080))?
+        .bind(("127.0.0.1", 8090))?
         .run()
         .await
 }
