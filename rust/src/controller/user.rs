5233020673accdf886546fe85f65d91906c01319
Bruce CHEN <805899926@qq.com>
2023-03-07 21:29:48 +0800

    #

--- rust/src/controller/user.rs
+++ rust/src/controller/user.rs
@@ -1,42 +1,48 @@
-use std::collections::HashMap;
-use actix_web::{get, web, Responder, Result, post};
+use actix_web::{get, post, web, Responder, Result};
 use serde::{Deserialize, Serialize};
 use serde_json::{json, Value};
+use std::collections::HashMap;
+use std::iter::Map;
 
 #[derive(Serialize, Deserialize)]
 struct User {
     name: String,
+    age: u64,
+    extra: HashMap<String, Value>,
 }
 
 #[post("/user/post1")]
 pub async fn post1(param: web::Json<HashMap<String, Value>>) -> Result<impl Responder> {
-    let _name = json!(param.get("name"));
+    let _name = param.get("name").unwrap().as_str().unwrap().to_string();
+    let _age = param.get("age").unwrap().as_u64().unwrap();
+    let _extra = param.get("extra").unwrap().as_object().unwrap();
+    let mut map_val = HashMap::new();
+    for t in _extra {
+        map_val.insert(t.0.clone(), t.1.clone());
+    }
     Ok(web::Json(User {
-        name: _name.to_string()
+        name: _name,
+        age: _age,
+        extra: map_val,
     }))
-}
-
-#[post("/user/post2")]
-pub async fn post2(param: web::Json<HashMap<String, Value>>) -> Result<impl Responder> {
-    let _age = param.get("age").unwrap();
-    println!("{}", _age);
-    Ok(web::Json(param))
 }
 
 #[get("/user/get1")]
 pub async fn get1(query: web::Query<HashMap<String, Value>>) -> Result<impl Responder> {
-    let _name = query.get("name").unwrap();
-    let _age = query.get("age").unwrap();
-    println!("{}", _name);
-    println!("{}", _age);
+    let _name: String = query.get("name").unwrap().as_str().unwrap().to_string();
+    let _age: u64 = query.get("age").unwrap().as_str().unwrap().parse::<u64>().unwrap();
     Ok(web::Json(User {
-        name: String::from("aaa")
+        name: _name,
+        age: _age,
+        extra: Default::default(),
     }))
 }
 
 #[get("/user/{name}")]
 pub async fn detail(name: web::Path<String>) -> Result<impl Responder> {
     Ok(web::Json(User {
-        name: name.to_string()
+        name: name.to_string(),
+        age: 89,
+        extra: Default::default(),
     }))
 }
--- rust/src/main.rs
+++ rust/src/main.rs
@@ -4,12 +4,13 @@ use actix_web::{App, HttpServer};
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| App::new()
-        .service(controller::user::post1)
-        .service(controller::user::post2)
-        .service(controller::user::get1)
-        .service(controller::user::detail))
-        .bind(("127.0.0.1", 8090))?
-        .run()
-        .await
+    HttpServer::new(|| {
+        App::new()
+            .service(controller::user::post1)
+            .service(controller::user::get1)
+            .service(controller::user::detail)
+    })
+    .bind(("127.0.0.1", 8080))?
+    .run()
+    .await
 }
