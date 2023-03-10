a9e60ef0bcfb7327c5e71f04270d5920007f2ceb
Bruce CHEN <805899926@qq.com>
2023-03-10 12:51:27 +0800

    #

--- java/infrastructure/src/main/java/com/czy/learning/infranstructure/util/MoneyUtil.java
+++ java/infrastructure/src/main/java/com/czy/learning/infranstructure/util/MoneyUtil.java
@@ -1,5 +1,7 @@
 package com.czy.learning.infranstructure.util;
 
+import org.apache.commons.lang3.StringUtils;
+
 import java.math.BigDecimal;
 import java.math.RoundingMode;
 
@@ -11,6 +13,9 @@ public class MoneyUtil {
      * @param val 元
      */
     public static Integer yuan2Fen(String val) {
+        if (StringUtils.isEmpty(val)) {
+            return null;
+        }
         BigDecimal val1 = new BigDecimal(val);
         BigDecimal val2 = val1.multiply(new BigDecimal(100)).setScale(0, RoundingMode.HALF_UP);
         return Integer.parseInt(val2.stripTrailingZeros().toPlainString());
@@ -22,6 +27,9 @@ public class MoneyUtil {
      * @param val 分
      */
     public static BigDecimal fen2Yuan(Integer val) {
+        if (val == null) {
+            return null;
+        }
         BigDecimal var1 = new BigDecimal(val);
         BigDecimal var2 = new BigDecimal(100);
         return var1.divide(var2, 2, RoundingMode.HALF_UP);
@@ -33,6 +41,9 @@ public class MoneyUtil {
      * @param val 分
      */
     public static String fen2YuanString(Integer val) {
+        if (val == null) {
+            return null;
+        }
         return fen2Yuan(val).stripTrailingZeros().toPlainString();
     }
 
