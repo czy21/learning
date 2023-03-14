8eba259d724ac82aa451d22c83eee78184a14cb3
Bruce CHEN <805899926@qq.com>
2023-03-14 11:15:16 +0800

    #

--- dotnet/Learning/Program.cs
+++ dotnet/Learning/Program.cs
@@ -9,16 +9,12 @@ var app = builder.Build();
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
 }
 
 app.UseRouting();
 
 app.UseAuthorization();
 
-app.MapControllerRoute(
-    name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+app.MapControllerRoute(name: "default", pattern: "{controller=Home}/{action=Index}/{id?}");
 
 app.Run();
