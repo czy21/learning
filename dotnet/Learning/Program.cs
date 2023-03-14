951a88a0b83fcc369f4798a923227399ca4b8393
Bruce CHEN <805899926@qq.com>
2023-03-14 15:40:13 +0800

    #

--- dotnet/Learning/Program.cs
+++ dotnet/Learning/Program.cs
@@ -6,9 +6,16 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+if (!builder.Environment.IsDevelopment())
+{
+    var configBasePath = Environment.GetEnvironmentVariable("APP_CONF");
+    if (configBasePath != null)
+    {
+        builder.Configuration.SetBasePath(configBasePath);
+    }
+}
 
 var app = builder.Build();
-
 var swaggerEnable = app.Configuration.GetValue<bool>("swagger:enable");
 if (swaggerEnable)
 {
