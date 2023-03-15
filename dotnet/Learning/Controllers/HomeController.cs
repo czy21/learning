7237ee79e823a82e9f4c64f05dbb204debcc7440
Bruce CHEN <805899926@qq.com>
2023-03-15 20:39:02 +0800

    #

--- dotnet/Learning/Controllers/HomeController.cs
+++ dotnet/Learning/Controllers/HomeController.cs
@@ -1,14 +1,19 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Consul;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using JsonSerializer = System.Text.Json.JsonSerializer;
 
 namespace Learning.Controllers;
 
 public class HomeController : Controller
 {
     private readonly ILogger<HomeController> _logger;
+    private readonly IConsulClient _consulClient;
 
-    public HomeController(ILogger<HomeController> logger)
+    public HomeController(ILogger<HomeController> logger, IConsulClient consulClient)
     {
         _logger = logger;
+        _consulClient = consulClient;
     }
 
     [HttpGet("list")]
@@ -16,14 +21,16 @@ public class HomeController : Controller
     {
         var dic = new Dictionary<string, object> { { "name", "czy" } };
         var list = new List<Dictionary<string, object>> { dic };
+        var kvPair = _consulClient.KV.Get("demo-common/application/data").Result.Response;
+        var p=kvPair.Value.GetValue(0);
         return Task.FromResult(list);
     }
 
-    [HttpGet(template:"list2")]
+    [HttpGet(template: "list2")]
     public Task<List<Dictionary<string, object>>> List2()
     {
         var dic = new Dictionary<string, object>() { { "name", "ha" } };
         _logger.LogInformation("hahaha");
-        return Task.FromResult(new List<Dictionary<string, object>> {dic});
+        return Task.FromResult(new List<Dictionary<string, object>> { dic });
     }
 }
--- dotnet/Learning/Program.cs
+++ dotnet/Learning/Program.cs
@@ -1,7 +1,14 @@
+using Consul;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
+builder.Services.AddSingleton<IConsulClient>(consul =>
+    new ConsulClient(consulConfig =>
+    {
+        consulConfig.Address = new Uri("http://consul.cluster.com");
+    }));
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
