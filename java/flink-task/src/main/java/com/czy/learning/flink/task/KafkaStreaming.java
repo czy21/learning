967b247a9d6dfe864d1cfd25b92ba543a1930dbb
Bruce CHEN <805899926@qq.com>
2023-03-16 15:17:24 +0800

    #

--- java/flink-task/src/main/java/com/czy/learning/flink/task/KafkaStreaming.java
+++ java/flink-task/src/main/java/com/czy/learning/flink/task/KafkaStreaming.java
@@ -2,31 +2,51 @@ package com.czy.learning.flink.task;
 
 
 import com.czy.learning.domain.entity.constant.QueueConstant;
-import com.czy.learning.flink.common.serialization.MapDeserializer;
+import org.apache.flink.api.common.eventtime.SerializableTimestampAssigner;
 import org.apache.flink.api.common.eventtime.WatermarkStrategy;
+import org.apache.flink.api.common.typeinfo.Types;
+import org.apache.flink.api.java.tuple.Tuple2;
 import org.apache.flink.connector.kafka.source.KafkaSource;
 import org.apache.flink.connector.kafka.source.enumerator.initializer.OffsetsInitializer;
-import org.apache.flink.connector.kafka.source.reader.deserializer.KafkaRecordDeserializationSchema;
+import org.apache.flink.formats.json.JsonDeserializationSchema;
 import org.apache.flink.streaming.api.datastream.DataStream;
+import org.apache.flink.streaming.api.datastream.KeyedStream;
+import org.apache.flink.streaming.api.datastream.WindowedStream;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.streaming.api.windowing.assigners.SlidingEventTimeWindows;
+import org.apache.flink.streaming.api.windowing.time.Time;
+import org.apache.flink.streaming.api.windowing.windows.TimeWindow;
 
-import java.util.Map;
+import java.time.Duration;
 
 public class KafkaStreaming {
 
     public static void main(String[] args) throws Exception {
 
         StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
-
-        KafkaSource<Map<String, Object>> source = KafkaSource.<Map<String, Object>>builder()
+        JsonDeserializationSchema<OrderDTO> orderDeserializer = new JsonDeserializationSchema<>(OrderDTO.class);
+        KafkaSource<OrderDTO> source = KafkaSource.<OrderDTO>builder()
                 .setBootstrapServers("192.168.2.18:9092,192.168.2.18:9093")
                 .setTopics("input5Topic")
                 .setGroupId(QueueConstant.GLOBAL_QUEUE_GROUP1)
                 .setStartingOffsets(OffsetsInitializer.latest())
-                .setDeserializer(KafkaRecordDeserializationSchema.valueOnly(MapDeserializer.class))
+                .setValueOnlyDeserializer(orderDeserializer)
                 .build();
-        DataStream<Map<String, Object>> stream = env.fromSource(source, WatermarkStrategy.noWatermarks(), "Kafka Source");
-        stream.print();
+        DataStream<OrderDTO> stream = env.fromSource(source, WatermarkStrategy.noWatermarks(), "Kafka Source");
+        DataStream<Tuple2<Long, Long>> orderNoTimeTuple = stream
+                .map(t -> Tuple2.of(t.getOrderNo(), t.getTime()))
+                .returns(Types.TUPLE(Types.LONG, Types.LONG));
+        DataStream<Tuple2<Long, Long>> orderNoTimeWaterLine = orderNoTimeTuple
+                .assignTimestampsAndWatermarks(
+                        WatermarkStrategy
+                                .<Tuple2<Long, Long>>forBoundedOutOfOrderness(Duration.ofSeconds(1))
+                                .withTimestampAssigner((SerializableTimestampAssigner<Tuple2<Long, Long>>) (element, recordTimestamp) -> element.f1)
+                )
+                .returns(Types.TUPLE(Types.LONG, Types.LONG));
+        DataStream<Tuple2<Long, Integer>> kvDS = orderNoTimeWaterLine.map(t -> Tuple2.of(t.f0, 1)).returns(Types.TUPLE(Types.LONG, Types.INT));
+        KeyedStream<Tuple2<Long, Integer>, Long> keyByDS = kvDS.keyBy(t -> t.f0);
+        WindowedStream<Tuple2<Long, Integer>, Long, TimeWindow> keyByDS1 = keyByDS.window(SlidingEventTimeWindows.of(Time.minutes(1), Time.seconds(5)));
+        keyByDS1.sum(1).print();
         env.execute("kafka source job 1");
     }
 
---
+++ java/flink-task/src/main/java/com/czy/learning/flink/task/OrderDTO.java
@@ -0,0 +1,31 @@
+package com.czy.learning.flink.task;
+
+public class OrderDTO {
+    private Long orderNo;
+    private Integer count;
+    private Long time;
+
+    public Long getOrderNo() {
+        return orderNo;
+    }
+
+    public void setOrderNo(Long orderNo) {
+        this.orderNo = orderNo;
+    }
+
+    public Integer getCount() {
+        return count;
+    }
+
+    public void setCount(Integer count) {
+        this.count = count;
+    }
+
+    public Long getTime() {
+        return time;
+    }
+
+    public void setTime(Long time) {
+        this.time = time;
+    }
+}
