8ca8468181d20fd085be3d8fb252f4180d9b2aca
Bruce CHEN <805899926@qq.com>
2023-03-17 13:18:10 +0800

    #

--- dotnet/Demo/Demo.Domain/UserPO.cs
+++ dotnet/Demo/Demo.Domain/UserPO.cs
@@ -2,9 +2,7 @@ using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Demo.Domain;
 
-public class UserPO
+public class UserPO : BaseEntity
 {
-    [Column(name: "id")] public string? Id { get; set; }
     [Column(name: "user_name")] public string? UserName { get; set; }
-    [Column(name: "create_time")] public DateTime? CreateTime { get; set; }
 }
