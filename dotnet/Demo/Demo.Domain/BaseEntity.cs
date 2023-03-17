7062cf6ae523caaa9a1245c26b3d4cbe0616918c
Bruce CHEN <805899926@qq.com>
2023-03-17 21:14:23 +0800

    #

--- dotnet/Demo/Demo.Domain/BaseEntity.cs
+++ dotnet/Demo/Demo.Domain/BaseEntity.cs
@@ -4,7 +4,7 @@ namespace Demo.Domain
 {
     public class BaseEntity<TId, TUser>
     {
-        [Column(name: "id")] public TId Id { get; set; }
+        [Column(name: "id")] public TId? Id { get; set; }
         [Column(name: "create_time")] public DateTime CreateTime { get; set; }
         [Column(name: "update_time")] public DateTime UpdateTime { get; set; }
         [Column(name: "create_user")] public TUser? CreateUser { get; set; }
